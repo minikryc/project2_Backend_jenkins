a6381524e92a5fd63251d898aa6736af189b54aa
minikryc <mini122600@gmail.com>
2025-04-01 11:18:18 +0900

    fix: User table name set

--- src/main/java/com/eouil/bank/bankapi/domains/User.java
+++ src/main/java/com/eouil/bank/bankapi/domains/User.java
@@ -1,13 +1,11 @@
 package com.eouil.bank.bankapi.domains;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name = "users")
 @Getter@Setter
 public class User {
     @Id @GeneratedValue
