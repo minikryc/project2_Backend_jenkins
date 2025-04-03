a62cece096a357235f99aa6f0eda09f08a5f5b99
yoursseul <yoursseul1107@daum.net>
2025-04-03 15:39:37 +0900

    refactor: Separate access and refresh token generation in JwtUtil

--- src/main/java/com/eouil/bank/bankapi/utils/JwtUtil.java
+++ src/main/java/com/eouil/bank/bankapi/utils/JwtUtil.java
@@ -11,29 +11,47 @@ import java.util.Date;
 public class JwtUtil {
     private static final String SECRET_KEY = "0123456789abcdef0123456789abcdef"; // HS256, 최소 32자(256bits) 이상
                                                                                 // 배포 시 환경변수로 관리해야 함
-    private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1시간
+    private static final long ACCESS_EXP = 1000 * 60 * 30;  // 30분
+    private static final long REFRESH_EXP = 1000 * 60 * 60 * 24 * 7; // 7일
 
     
     private static final Key KEY = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 
-    // userId로 토큰 생성
-    public static String generateToken(String userId) {
+    // access token
+    public static String generateAccessToken(String userId) {
         return Jwts.builder()
-                .setSubject(userId)  // userId로 토큰 생성
+                .setSubject(userId)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_EXP))
+                .signWith(KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // refresh token
+    public static String generateRefreshToken(String userId) {
+        return Jwts.builder()
+                .setSubject(userId)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + REFRESH_EXP))
                 .signWith(KEY, SignatureAlgorithm.HS256)
                 .compact();
     }
 
     // JWT 토큰을 검증하고 userId 추출
     public static String validateTokenAndGetUserId(String token) {
-        Claims claims = Jwts.parserBuilder()
-                .setSigningKey(KEY)
-                .build()
-                .parseClaimsJws(token.replace("Bearer ", ""))
-                .getBody();
-
-        return claims.getSubject();
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(KEY)
+                    .build()
+                    .parseClaimsJws(token.replace("Bearer ", ""))
+                    .getBody();
+    
+            return claims.getSubject();
+        } catch (io.jsonwebtoken.ExpiredJwtException e) {
+            throw new RuntimeException("Token expired");
+        } catch (Exception e) {
+            throw new RuntimeException("Invalid token");
+        }
     }
+    
 }
