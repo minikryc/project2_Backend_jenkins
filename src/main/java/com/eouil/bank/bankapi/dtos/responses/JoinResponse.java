8e125cff8df8b8efceab64626b58729792a801c5
minikryc <mini122600@gmail.com>
2025-04-05 13:17:31 +0900

    fix: joinResponse edited in /join

--- src/main/java/com/eouil/bank/bankapi/dtos/responses/JoinResponse.java
+++ src/main/java/com/eouil/bank/bankapi/dtos/responses/JoinResponse.java
@@ -1,12 +1,10 @@
 package com.eouil.bank.bankapi.dtos.responses;
 
 public class JoinResponse {
-    public String userId;
     public String name;
     public String email;
 
-    public JoinResponse(String userId, String name, String email) {
-        this.userId = userId;
+    public JoinResponse(String name, String email) {
         this.name = name;
         this.email = email;
     }
