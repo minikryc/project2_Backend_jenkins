4d77762818f0391334307c3d18370861cf1b5c70
Yuhwan Kim <yhkim8046@gmail.com>
2025-04-10 16:45:24 +0900

    - feat(config): config updates

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
+# 1. Java 17 이미지 (x86 아키텍처용)
 FROM openjdk:17-jdk-slim
 
+# 2. 작업 디렉터리
 WORKDIR /app
 
-VOLUME /tmp
+# 3. jar 파일 복사 (이름 명시적으로 지정!)
+COPY build/libs/*.jar app.jar
 
-ARG JAR_FILE=build/libs/*.jar
-COPY ${JAR_FILE} app.jar
-COPY .env .env
-
+# 4. 포트 오픈
 EXPOSE 8080
 
+# 5. 애플리케이션 실행
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/eouil/bank/bankapi/BankapiApplication.java
+++ src/main/java/com/eouil/bank/bankapi/BankapiApplication.java
@@ -4,12 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  BankapiApplication {
-
+public class BankapiApplication {
 	public static void main(String[] args) {
-		io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(BankapiApplication.class, args);
 	}
-
 }
