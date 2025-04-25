a9123408b05a28e05952c02d790b39505e01a148
minikryc <mini122600@gmail.com>
2025-04-25 12:07:37 +0900

    feat: secret key changed and set as env variable

--- src/main/java/com/eouil/bank/bankapi/utils/JwtUtil.java
+++ src/main/java/com/eouil/bank/bankapi/utils/JwtUtil.java
@@ -4,17 +4,22 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.Claims;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.security.Key;
 import java.util.Date;
 
+@Component
 public class JwtUtil {
     private final Key key;
     private final long ACCESS_EXP = 1000 * 60 * 5;  // 5분
     private final long REFRESH_EXP = 1000 * 60 * 60 * 24 * 7; // 7일
 
-    
-    private static final Key KEY = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+    public JwtUtil(@Value("${jwt.secret}") String secretKey) {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
 
     // access token
     public String generateAccessToken(String userId) {
@@ -22,7 +27,7 @@ public class JwtUtil {
                 .setSubject(userId)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + ACCESS_EXP))
-                .signWith(KEY, SignatureAlgorithm.HS256)
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
@@ -32,14 +37,14 @@ public class JwtUtil {
                 .setSubject(userId)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + REFRESH_EXP))
-                .signWith(KEY, SignatureAlgorithm.HS256)
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
     // JWT 토큰을 검증하고 userId 추출
     public String validateTokenAndGetUserId(String token) {
         Claims claims = Jwts.parserBuilder()
-                .setSigningKey(KEY)
+                .setSigningKey(key)
                 .setAllowedClockSkewSeconds(60) // ← 1분 정도 시간 차 허용
                 .build()
                 .parseClaimsJws(token.replace("Bearer ", ""))
